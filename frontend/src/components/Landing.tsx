1d6742bf53025df056f55d819b2392d3b2c081fe
divu777 <E21CSEU0721@bennett.edu.in>
2024-06-20 23:47:39 +0530

    added readme

--- frontend/src/components/Landing.tsx
+++ frontend/src/components/Landing.tsx
@@ -24,9 +24,11 @@ const Landing = () => {
         </div>
         <div className="flex flex-grow w-2/4 flex-col gap-5 ">
           <div className=" flex flex-col  h-1/3  bg-gray-900 rounded-3xl justify-evenly items-center">
-            <div className="flex flex-col justify-center items-center">
-              <h3 className="text-2xl font-bold mb-2">Add Captions Easily</h3>
-              <p className="text-sm">
+            <div className="flex flex-col justify-center items-center bg-gray-900">
+              <h3 className="text-2xl font-bold mb-2 bg-gray-900">
+                Add Captions Easily
+              </h3>
+              <p className="text-sm bg-gray-900">
                 Upload your video and get clear captions in minutes. No editing
                 software required!
               </p>
